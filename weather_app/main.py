7972b814620b699b698920ff0e6f00f602d3d562
t0m0h1 <thomashathawayhogg@gmail.com>
2024-01-19 15:47:57 +0000

    boilerplate

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,3 +1,20 @@
+# a very basic weather app using tkinter and bs4
+
 import tkinter as tk
 import requests
-import beautifulsoup4 as bs
+import bs4 as bs  # BeautifulSoup4
+
+# create a window
+root = tk.Tk()
+
+# set the title
+root.title("Weather App")
+
+# set the size of the window
+root.geometry("400x400")
+
+# create a label
+label1 = tk.Label(root, text="Enter the city: ")
+label1.pack()
+
+root.mainloop()
