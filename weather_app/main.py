680f32070b2180f99fcada453eabfe5c1e1cadd9
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-01-21 18:29:41 +0000

    changed functions

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,6 +1,7 @@
 # a very basic weather app using tkinter and bs4
 
 import tkinter as tk
+from tkinter import messagebox # to show error message later on
 import requests
 import bs4 as bs  # BeautifulSoup4
 from configparser import ConfigParser # to read the api key from the config file
@@ -31,35 +32,34 @@ root.geometry("400x400")
 
 def get_weather(city):
     result = requests.get(api_url.format(city, api_key))
-    if result:
-        json = result.json()
-        # city, country, temp_celsius, temp_fahrenheit, icon, weather etc
-        city = json["name"]
-        country = json["sys"]["country"]
-        temp_kelvin = json["main"]["temp"] # temperature in kelvin to be converted to celsius
-        temp_celsius = temp_kelvin - 273.15
-        temp_farenheit = (temp_kelvin - 273.15) * 9/5 + 32 # temperature conversion formula
-        icon = json["weather"][0]["icon"]
-        weather = json["weather"][0]["main"]
-        final = (city, country, temp_celsius, temp_farenheit, icon, weather)
-        return final # return the tuple
-    else:
-        return None
+    if result.status_code != 200:  # If the status code is not 200, the request was not successful
+        raise Exception(f"Error: Received status code {result.status_code} from OpenWeatherMap API")
+    json = result.json()
+    # city, country, temp_celsius, temp_fahrenheit, icon, weather etc
+    city = json["name"]
+    country = json["sys"]["country"]
+    temp_kelvin = json["main"]["temp"] # temperature in kelvin to be converted to celsius
+    temp_celsius = temp_kelvin - 273.15
+    temp_farenheit = (temp_kelvin - 273.15) * 9/5 + 32 # temperature conversion formula
+    icon = json["weather"][0]["icon"]
+    weather = json["weather"][0]["main"]
+    final = (city, country, temp_celsius, temp_farenheit, icon, weather)
+    return final # return the tuple
 
-print(get_weather("London"))
 
 def search():
     city = city_text.get()
-    weather = get_weather(city)
-    if weather:
+    try:
+        weather = get_weather(city)
         location_label["text"] = f"{weather[0]}, {weather[1]}"
         image["bitmap"] = "images/{}.png".format(weather[4])
         temperature_label["text"] = f"{weather[2]:.2f}°C, {weather[3]:.2f}°F"
         weather_label["text"] = weather[5]
-    else:
-        tk.messagebox.showerror(f"Error", "Cannot find city {city}")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
- 
+
+
 # create a label
 label1 = tk.Label(root, text="Enter the city: ")
 label1.pack()
@@ -89,6 +89,10 @@ temperature_label.pack()
 # create a label for the weather condition
 weather_label = tk.Label(root, text="")
 weather_label.pack()
+
+# check that the api key is correctly read from the config file
+api_key = section["api_key"]
+print(api_key)
 
 
 # run the app
