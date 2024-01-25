f8d59ed1578062d061c27ea88e673dc97a237d51
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-01-25 17:41:40 +0000

    changes

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -13,15 +13,15 @@ def add_expenses():
     expenses_entry.delete(0, tk.END)
     print(expense_list)
     print(np.sum(expense_list))
-    expenses_label.config(text=f"Expenses:  £ {np.sum(expense_list)}")
-    expenses_label.grid(column=1, row=4)
+    total_expenses_label.config(text=f"Total:  £ {np.sum(expense_list)}")
+    total_expenses_label.grid(column=1, row=4)
     expenses_entry.grid(column=1, row=1)
-    add_expenses_button.grid(column=1, row=3)
+    add_expenses_button.place(x=0, y=90)
     return expense_list
 
 
 def clear():
-    expenses_label.config(text="Expenses: ")
+    total_expenses_label.config(text="Expenses: ")
 
 
 
@@ -29,14 +29,14 @@ add_expenses_label = tk.Label(root, text="Add Expenses :", font=("Helvetica", 16
 add_expenses_label.grid(column=1, row=0, pady=10)
 
 
-expenses_label = tk.Label(root, text="", font=("Helvetica", 16))
+total_expenses_label = tk.Label(root, text="", font=("Helvetica", 16))
 
 
 expenses_entry = tk.Entry(root, font=("Helvetica", 16))
 expenses_entry.grid(column=1, row=1, columnspan=2, pady=10)
 
 add_expenses_button = tk.Button(root, text="Add", font=("Helvetica", 16), command=add_expenses)
-add_expenses_button.grid(column=1, row=3)
+add_expenses_button.place(x=0, y=90)
 
 
 clear_button = tk.Button(root, text="Clear", font=("Helvetica", 16), command=clear)
