1906384a5ceea5054c59244ee2c520e5282980d3
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-01-25 17:21:58 +0000

    changes

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -1,55 +1,41 @@
 import tkinter as tk
 import numpy as np
-import sqlite3
 
-# tkinter expense tracker: add expenses, view expenses, delete expenses, save expenses
-
-
-
-
-# create the main window and set the title
 root = tk.Tk()
-root.title("Expense Tracker")
-root.geometry("400x400")
-root.resizable(False, False)
+expense_list = []
+
+def add_expenses():
+    expenses = expenses_entry.get()
+    expense_list.append(float(expenses))  # convert string to float before appending
+    expenses_entry.delete(0, tk.END)
+    print(expense_list)
+    print(np.sum(expense_list))
+    expenses_label.config(text=f"Expenses:  £ {np.sum(expense_list)}")
+    expenses_label.grid(column=0, row=0)
+    expenses_entry.grid(column=1, row=0)
+    add_expenses_button.grid(column=1, row=3)
+    return expense_list
 
 
-# functions
-def total_expenses():
-    expense_list = []
-    expense_list.append(float(expenses_entry.get()))
-    total = np.sum(expense_list)
-    total_expenses_label.config(text="£" + str(total))
+def clear():
+    expenses_label.config(text="Expenses: ")
 
 
-# labels
-expenses_label = tk.Label(root, text="Enter Expenses:", font=("Helvetica", 16))
-total_expenses_label = tk.Label(root, text="Total Expenses:", font=("Helvetica", 16))
 
-# grid the labels
+
+expenses_label = tk.Label(root, text="Expenses: ", font=("Helvetica", 16))
 expenses_label.grid(column=0, row=0)
-total_expenses_label.grid(column=0, row=1)
-
-
-
-
-
-
-
 
 expenses_entry = tk.Entry(root, font=("Helvetica", 16))
 expenses_entry.grid(column=1, row=0)
 
+add_expenses_button = tk.Button(root, text="Add", font=("Helvetica", 16), command=add_expenses)
+add_expenses_button.grid(column=1, row=3)
 
-submit_button = tk.Button(root, text="Submit", font=("Helvetica", 16), command=total_expenses)
-submit_button.grid(column=0, row=2)
+total_expenses_label = tk.Label(root, text=f'{expense_list}', font=("Helvetica", 16))
+total_expenses_label.grid(column=0, row=4)
 
-
-
-
-
-
-
+clear_button = tk.Button(root, text="Clear", font=("Helvetica", 16), command=clear)
+clear_button.grid(column=1, row=4)
 
 root.mainloop()
-
